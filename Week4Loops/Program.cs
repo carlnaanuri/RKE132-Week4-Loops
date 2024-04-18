69546d4c9ec2eaed4b5a8888dd976c37f970b0d8
carlnaanuri <naanuri@hotmail.com>
2024-04-18 13:47:49 +0300

    week4 code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿//FOR-LOOP
+//DRY-principle - Do not repeat yourself
+/*int i = 0;
+for(i = 0; i < 5; i++) //i++ = i + i, samm.  i = 0 on algus. i < 5 seni kestab kuni on väiksem, kui 5
+{
+    Console.WriteLine("We don't need no education.");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day.");*/
+
+
+
+string userPIN;
+while (true)
+{
+    Console.WriteLine("Enter your PIN");
+    userPIN = Console.ReadLine();
+    if (userPIN == "1234")
+    {
+        Console.WriteLine("Welcome!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong PIN. Try again.");
+    }
+}
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+bool loopActive = true; //bool võib olla true/false
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu has generated: {cpuRandom}");
+    Console.WriteLine("Make a guess. Enter a number from 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+    
+    if(userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        loopActive = false; //Kui on õige vastus valitud, siis programm edasi ei küsi sama küsimust
+    }
+    else
+    {
+        Console.WriteLine("Oops. Try again!");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
